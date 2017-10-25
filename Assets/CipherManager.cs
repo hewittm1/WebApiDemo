a521fed651f66dd525b9273686c8ea2023db1988
hewittm1 <hewittm1@gmatc.matc.edu>
2017-10-25 02:21:29 -0500

    first commit

---
+++ Assets/CipherManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class CipherManager : MonoBehaviour {
+    public Text text;
+    public int num;
+    public string cipher;
+    private string encodedString = "";
+    //private char[] charArr = new char[26];//['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    void Start () {
+        //charArr = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        string[] cipherArrary = Regex.Split(cipher, " ");
+        for(int i = 0; i < cipherArrary.Length; i++)
+        {
+            encodedString += Cipher(cipherArrary[i]);
+        }
+        text.text = encodedString.ToString();
+	}
+	private string Cipher(string _s)
+    {
+        print(_s);
+
+        char[] originalArr = _s.ToCharArray();
+        int firstCharId = 'a';
+        int offset = ('z' - 'a') + 1;
+        for (int i = 0; i < originalArr.Length; i++)
+        {
+            char oldId = originalArr[i];
+            int oldIdInAlphabet = oldId - firstCharId;
+            int newId = (oldIdInAlphabet + num) % offset;
+            char newCharId = (char)(firstCharId + newId);
+            originalArr[i] = newCharId;
+        }
+        return _s = " " + new string(originalArr);
+    }
+}
---
+++ Assets/DataLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+    string createUserURL = "http://gamedevee.com/InsertUser.php";
+    public string inputUserName;
+
+    //void Start()
+    //{
+    //    CreateUser(inputUserName);
+    //}
+    public void CreateUser(string username)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        WWW www = new WWW(createUserURL, form);
+        CreateNewUserScore(username);
+    }
+    private void CreateNewUserScore(string username)
+    {
+        string _createUserURL = "http://gamedevee.com/InsertUserScore.php";
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("userScorePost", 0);
+        WWW www = new WWW(_createUserURL, form);
+    }
+}
